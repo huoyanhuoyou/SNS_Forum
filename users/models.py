719bd32623924b5cec11353518b183c761e1214c
Moxie <129675312+huoyanhuoyou@users.noreply.github.com>
2023-10-30 16:01:55 +0800

    Add files via upload

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)  # 头像
+    avatar = models.ImageField(upload_to='avatars/%Y/%m/%d/', null=True, blank=True,default="path-to-default-avatar.jpg")  # 头像
     Sno = models.CharField(max_length=20, unique=True)  # 学号
     phone = models.CharField(max_length=15, blank=True, null=True)  # 电话
     email = models.EmailField(unique=True, verbose_name="邮箱")
--- users/urls.py
+++ users/urls.py
@@ -10,14 +10,14 @@ app_name = 'users'
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('register', views.register, name='register'),
-
     path('send_verification_code/', views.send_verification_code, name='send_verification_code'),
     path('update_sno/', views.update_sno, name='update_sno'),
     path('update_phone/', views.update_phone, name='update_phone'),
     path('update_email/', views.update_email, name='update_email'),
     path('update_name/', views.update_name, name='update_name'),
-
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('update_sno/', views.update_sno, name='update_sno'),
+    path('update_phone/', views.update_phone, name='update_phone'),
+    path('update_email/', views.update_email, name='update_email'),
+    path('update_name/', views.update_name, name='update_name'),
+    #path('my-image/',views.my_image,name = "my_image")
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
