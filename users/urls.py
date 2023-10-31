5a5a7e41f343fbf705fca39dcf15e5530690bc8b
zhouzhouzhouxiao <137688717+zhouzhouzhouxiao@users.noreply.github.com>
2023-10-31 20:30:41 +0800

    Add files via upload

--- users/urls.py
+++ users/urls.py
@@ -1,23 +1,10 @@
 from django.urls import path, include
-from users import views
-from django.conf import settings
-from django.conf.urls.static import static
+from chat import views
 
-# ... 你的其他URL配置 ...
-
-
-app_name = 'users'
+app_name = 'chat'
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
-    path('register', views.register, name='register'),
-    path('send_verification_code/', views.send_verification_code, name='send_verification_code'),
-    path('update_sno/', views.update_sno, name='update_sno'),
-    path('update_phone/', views.update_phone, name='update_phone'),
-    path('update_email/', views.update_email, name='update_email'),
-    path('update_name/', views.update_name, name='update_name'),
-    path('update_sno/', views.update_sno, name='update_sno'),
-    path('update_phone/', views.update_phone, name='update_phone'),
-    path('update_email/', views.update_email, name='update_email'),
-    path('update_name/', views.update_name, name='update_name'),
-    #path('my-image/',views.my_image,name = "my_image")
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # 私信发送页面
+    path('chat_message/sender:<str:sendername>/receiver:<str:receivername>', views.chat_message, name='chat_message'),
+    # 个人信息页面
+    path('my_info/<str:username>', views.my_info, name='my_info'),
+]
